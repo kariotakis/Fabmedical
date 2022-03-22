d8ae315ef86fd4ab16ee06b7c20d9950b020d160
kariotakis <george_kariotakis@hotmail.com>
2022-03-22 16:45:26 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("16d06574-499f-4e77-b0a7-ff903c0f6ff9");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
